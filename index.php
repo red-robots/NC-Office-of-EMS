4b18b729cd9ba6f6ce637fa3f25fe8123fc2d7eb
accrane <austincrane@Austins-MBP-2.lan>
2020-12-18 10:03:54 -0500

    fix homepage

--- index.php
+++ index.php
@@ -16,11 +16,11 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<?php if ( have_posts() ) : the_post();
+			<?php //if ( have_posts() ) : the_post();
 
 				get_template_part( 'template-parts/content', 'index' );
 
-			endif; ?>
+			//endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- template-parts/content-index.php
+++ template-parts/content-index.php
@@ -5,7 +5,7 @@
  */
 
 ?>
-
+yelo
 <article id="post-<?php the_ID(); ?>" <?php post_class( "template-index" ); ?>>
 	<?php get_template_part( 'template-parts/slides', 'index' ); ?>
     <div class="row-2">
